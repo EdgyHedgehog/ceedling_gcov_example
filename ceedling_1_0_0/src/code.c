70a4096a87922c5eb9eb6e92bb0a2670adc16286
Mike <kalbar.regular@gmail.com>
2024-11-26 22:54:36 +0100

    Added Ceedling 1.0.0 example.

---
+++ ceedling_1_0_0/src/code.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : ceedling_temp.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdbool.h>
+
+#include "code.h"
+
+bool function(void)
+{
+	return true;
+}
---
+++ ceedling_1_0_0/test/test_main.c
@@ -0,0 +1,26 @@
+
+#include "unity.h"
+
+#include <stdbool.h>
+
+#include "code.h"
+
+
+void setUp(void)
+{
+
+}
+
+
+void tearDown(void)
+{
+
+}
+
+
+void test_test01(void)
+{
+	TEST_ASSERT_FALSE(function());
+}
+
+
